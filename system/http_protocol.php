c1fa01a36f9b2f8b6ef43d1eed26129e04de2bac
mutian <bhfqlz@qq.com>
2022-11-27 15:28:33 +0800

    notices:
    version 2.0.7

--- system/http_protocol.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function http_header_template():void{
-    header("text/json;charset=UTF-8");
-}
-
-function http_status_setter(int $code):void{
-    header($GLOBALS['http_code'][$code]);
-}
